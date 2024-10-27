251ae68864b4dffdb7dd5a10a5aa19fd1e6d5a46
ignatiusfernand <igarciacarranza1081@ceuandalucia.es>
2024-10-27 14:20:21 +0100

    Canela en Rama
    fkfkfkfk

---
+++ Proyecto_3_3/src/ejercicios/Clase3_3A.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+public class Clase3_3A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Práctica3_3 + Ignacio García-Carranza");
+	}
+
+}
---
+++ Proyecto_3_3/src/module-info.java
@@ -0,0 +1,2 @@
+module Proyecto_3_3 {
+}
